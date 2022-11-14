df64ecccfcf7a8d3b747698147f4b699d8948010
corruptedash <corruptedash@outlook.com>
2022-11-14 12:21:19 +0800

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,32 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+PATH = 'db/jobs.sqlite'
+
 app=Flask(__name__)
+
+def open_connection():
+  connection = getattr(g, '_connection', None)
+  if connection == None:
+    connection = g._connection = sqlite3.connect(PATH)
+  connection.row_factory = sqlite3.Row
+  return connection
+
+def excute_sql(sql, value=(), commit=False, single=False):
+  connection = open_connection()
+  cursor = connection.excute(sql, value)
+  if commit == True:
+    results = connection.commit()
+  else:
+    results = cursor.fetchone() if single else cursor.fetchall()
+  cursor.close()
+  return results
+
+@app.teardown_appcontext
+def close_connection():
+  connection = getattr(g, '_connection', None)
+  if connection is not None:
+    connection.close()
 
 @app.route('/')
 @app.route('/jobs')
